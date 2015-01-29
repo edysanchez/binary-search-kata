bfb7552c12e9d70a7513fcea16d0e9ab03a771f4
Edgar Sanchez <edgar.sanchez@atrapalo.com>
2015-01-30 00:26:50 +0100

    fix commit steps script

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 bin/behat
 res=$?
+echo $1
 if [ $res -eq 0 ] 
 then
     git add features/searcher.feature
     git add src/Searcher/Searcher.php
-    git commit -m $1
+    git commit -m '$1'
 fi;
